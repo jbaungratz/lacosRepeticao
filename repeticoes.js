26c045659a42ee14610d307ba96f15acc060dc63
JBaungratz <jose1ab@hotmail.com>
2022-01-27 17:53:33 -0300

    tipos de laços em Java finalizado

---
+++ repeticoes.js
@@ -0,0 +1,25 @@
+// laço contado -> For
+
+// for(let contador = 0; contador < 5; contador++) {
+//     console.log("Teste", contador)
+// }
+
+
+// for(let numero = 10; numero > 0; numero--) {
+//     console.log(numero)
+// }
+
+// let listaAlunos = ["Guilherme", "João", "Julielson", "Jéssica", "Denise"]
+
+// for(let contador = 0; contador < 5; contador++) {
+//     console.log(`Boa Tarde ${listaAlunos[contador]}`)
+// }
+
+//LAÇO CONDICIONAL
+
+let nomeUsuario = "Thiago"
+let soma = 0
+
+while(nomeUsuario == "Thiago") {
+    console.log("Boa tarde Thiago")
+}
